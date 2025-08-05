686e9be6b23084001dec2350a51b726ff4147463
deepak-respirer <deepak@respirer.in>
2025-08-05 16:17:36 +0530

    setting static path is changed

--- event/settings.py
+++ event/settings.py
@@ -196,6 +196,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_URL = 'static/'
 STATIC_ROOT = '/var/www/booking/static/'
 
 # Default primary key field type
